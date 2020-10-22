4b4384ae6903a2f7298558873e4bc4ad7dc7432d
John Samuel <johnsamuelwrites@gmail.com>
2020-10-22 11:52:19 +0200

    Add example on long double values

---
+++ c/algorithmes/2020/longdouble.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  long double ld = 3.14E2;
+  printf("long double ld %Lg\n", ld);
+  ld = 3.14E-2;
+  printf("long double ld %Lg\n", ld);
+  ld = 3.14E-6;
+  printf("long double ld %Lg\n", ld);
+  ld = 7.88E-6;
+  printf("long double ld %Lg\n", ld);
+  return (0);
+}
