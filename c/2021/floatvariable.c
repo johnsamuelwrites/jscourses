227911865b21f42d1e04f32438073249753fc5f5
John Samuel <johnsamuelwrites@gmail.com>
2021-09-16 14:24:23 +0200

    Update example with printf

--- c/2021/floatvariable.c
+++ c/2021/floatvariable.c
@@ -6,8 +6,23 @@
 
 int main() {
     float pi = 3.14;
-    printf("Bonjour %f\n", pi);
-    printf("Bonjour %.2f\n", pi);
-    printf("Bonjour %2.2f\n", pi);
+    printf("pi %f\n", pi);
+    printf("pi %.2f\n", pi);
+    printf("pi %2.2f\n", pi);
+ 
+    float valeur = 12345.6789;
+    printf("valeur :%f\n", valeur);
+    printf("valeur :%1.2f\n", valeur);
+    printf("valeur :%2.2f\n", valeur);
+    printf("valeur :%3.2f\n", valeur);
+    printf("valeur :%4.2f\n", valeur);
+    printf("valeur :%5.2f\n", valeur);
+    printf("valeur :%6.2f\n", valeur);
+    printf("valeur :%7.2f\n", valeur);
+    printf("valeur :%8.2f\n", valeur);
+    printf("valeur :%9.2f\n", valeur);
+    printf("valeur :%9.3f\n", valeur);
+    printf("valeur :%14.4f\n", valeur);
+
     return (0);
 }
