0f6e737911f190b330c1c2e629b6bbd25ca87cff
John Samuel <johnsamuelwrites@gmail.com>
2024-09-04 16:12:18 +0200

    Add examples discussed in class

---
+++ c/PLP/2024/bonjour.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+   int annee = 2024;
+   printf("Bonjour !! C'est l'année %d\n", annee);
+   return 0;
+}
---
+++ c/PLP/2024/sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   int annee = 2024;
+   long long int lli = 2024;
+   printf("sizeof(annee): %lu\n", sizeof(annee));
+   printf("sizeof(lli): %lu\n", sizeof(lli));
+   return 0;
+}
