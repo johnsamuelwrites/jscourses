2353b55559555832a31ec59e0bf033054d0841d2
John Samuel <johnsamuelwrites@gmail.com>
2024-09-10 11:25:10 +0200

    Add example code from class 1

---
+++ c/2024/bonjour.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("Bonjour\n");
+	return 0;
+}
---
+++ c/2024/variable.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	int a = 2024;
+
+	printf("Bonjour %d\n", a);
+	return 0;
+}
